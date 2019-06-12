af099422b6a4976ccfb1f17697b1d6626562117d
Liz Kelley and Marc Davies <liz@>
2019-06-12 11:39:11 -0700

    add loop to display list of heroes and click to edit functionality

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Elly McTeacher' },
+  { id: 12, name: 'Jared the Jenius' },
+  { id: 13, name: 'Mike McSolo' },
+  { id: 14, name: 'Babbling Brooke' },
+  { id: 15, name: 'Clumsy Reese' },
+  { id: 16, name: 'Disco Stu' },
+  { id: 17, name: 'Dark Mavies' },
+  { id: 18, name: 'Odd Todd' },
+  { id: 19, name: 'Nice Neil' },
+  { id: 20, name: 'Eagle Eye' }
 ];
